5acbad9eca1e239835f62b47e69e7f92253cda47
Simon_Muchemi <simonkenya846@gmail.com>
2024-03-11 19:19:45 +0000

    photo updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Simon Muchemi",
+  title: "Full Stack Developer",
+  email: "muchemi.developer@gmail.com",
+  gitHub: "SymonMuchemi",
+  instagram: "SymonMuchemi",
+  linkedIn: "Simon Muchemi",
   medium: "",
-  twitter: "microsoft",
+  twitter: "SymonMuchemi",
   youTube: "Code",
 };
 
